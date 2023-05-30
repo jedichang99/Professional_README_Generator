9f89129462149aa2eae772899e7ea847887520d2
Jedidiah Chang <jedichang99@gmail.com>
2023-05-30 17:22:11 -0500

    added JS

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Professional_README_Generator
+
+## Description
+
+The purpose of this project was to display the power of Java Script and API's. I used a weather API that displays the wether acording to the city that you input.
+
+## Table of Contents
+
+- [Professional\_README\_Generator](#professional_readme_generator)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+Now you can go about the web page and test your knologe of JS. Try to find the weather for the city you live in. You can go the website with this URL.
+<https://jedichang99.github.io/Code-Quiz/>
+
+![The Whole Page](assets/photos/whole_page.png)
+![Pre-saved citys](assets/photos/pre-set_citys.png)
+![Enter a city in this box](assets/photos/Enter_a_city.png)
+![Forcast Section](assets/photos/forcast.png)
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.
---
+++ index.js
@@ -0,0 +1,102 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// Function to generate the README file
+function generateREADME(answers) {
+  return `
+# ${answers.title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+This application is covered under the ${answers.license} license. ![License Badge](https://img.shields.io/badge/License-${answers.license}-green)
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+For any questions or concerns, please reach out to me at ${answers.email}. You can also visit my GitHub profile: [${answers.username}](https://github.com/${answers.username}).
+  `;
+}
+
+// Prompt the user for input
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the title of your project:",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter a description:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Enter installation instructions:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Enter usage information:",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Choose a license for your application:",
+      choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3-Clause", "None"],
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "Enter contribution guidelines:",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Enter test instructions:",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "Enter your GitHub username:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address:",
+    },
+    console.log(this),
+  ])
+  .then((answers) => {
+    const readmeContent = generateREADME(answers);
+
+    fs.writeFile("README.md", readmeContent, (err) => {
+      if (err) throw err;
+      console.log("README.md file has been created successfully!");
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
